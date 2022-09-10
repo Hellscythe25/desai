fe71b13ccad92e0139c3d75abaefa36c6fa0a463
Hellscythe25 <46532421+Hellscythe25@users.noreply.github.com>
2022-09-10 09:50:55 -0500

    Class 2: jdbc mantemaintance

--- src/main/java/com/isil/jdbc/MainApp.java
+++ src/main/java/com/isil/jdbc/MainApp.java
@@ -1,10 +1,7 @@
 package com.isil.jdbc;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainApp {
 
@@ -16,11 +13,46 @@ public class MainApp {
         Connection con = DriverManager.getConnection(jdbcUrl, "root", "root1234");
 
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select * from users");
+        //Insertar
+        //int filasAfectadas = st.executeUpdate("insert into Users values (4, 'Juan', '999999998', 'Vancuver')");
+        //Actualizar
+        //int filasAfectadas = st.executeUpdate("update Users set name='Christian' where name='Cueva'");
+        //Eliminar
+        //int filasAfectadas = st.executeUpdate("delete from Users where idUser=4");
+
+        //Mantenimiento
+        /*
+        PreparedStatement ps = con.prepareStatement("insert into Users values(?,?,?,?)");
+        ps.setInt(1,4);
+        ps.setString(2, "Beto");
+        ps.setString(3, "998998998");
+        ps.setString(4, "Bogota");
+
+
+
+        int filasAfectadas = ps.executeUpdate();
+        */
+        //Consulta
+
+        PreparedStatement ps2 = con.prepareStatement("SELECT * FROM USERS WHERE idUser=?");
+        ps2.setInt(1, 3);
+
+
+
+
+        //ResultSet rs = st.executeQuery("select * from users");
+        ResultSet rs = ps2.executeQuery();
+
 
         while(rs.next()){
             System.out.println(rs.getString("idUser") + " " + rs.getString("name") + " " + rs.getString("phone") + " " + rs.getString("city"));
         }
+
+
+
+
+
+
 
     }
 
